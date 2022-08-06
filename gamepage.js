93573588f2c45167938499f4c63c4a17d9706b63
kartboiguthub <100388087+kartboiguthub@users.noreply.github.com>
2022-08-06 14:50:29 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,10 @@
+// random value generated
+var y = Math.floor(Math.random() * 10 + 1)
+  
+// counting the number of guesses
+
+
+// made for correct Guess
+
+  
+// function for number guessed by user     
